36618fb01ed75676c59a100c67d0a5138e195489
Rémi <remi.dandois@gmail.com>
2023-09-05 16:44:14 -0400

    reports routes management added and API call after exec removed

--- functions/reports.js
+++ functions/reports.js
@@ -1,22 +1,25 @@
+const {globSync} = require("glob")
+const fs = require("fs")
+
 require('dotenv').config();
 apiQaIntUrl = process.env.API_QA_INT_URL
 
 module.exports = {
-    sendResultsToApiQa : async (results) => {
-        return new Promise((res, rej) => {
-            fetch(`${apiQaIntUrl}/reports/runner/cypress`, {
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(results)
-            }).then((response) => response.text()).then((response) => {
-                console.log({ response: response, typeof: typeof response });
-                res({ msg: response })
-            }).catch(err => {
-                rej({status: "err", log: err, msg: "execution finished but an error occured when trying to send results to api-qa"})
-            })
-        })
+    getReports: (app) => {
+        var f = []
+        files = globSync(`cypress/reports/${app}*.json`)
+        files.forEach(file => {
+            f.push(file.slice("cypress/reports/".length, file.length-".json".length))
+        });
+        return {status: 200, msg: `report folder scanned for ${app} reports`, reports: f}
+    },
+    getReport: (reportName) => {
+        return JSON.parse(fs.readFileSync(`cypress/reports/${reportName}.json`, "utf-8"))
+    },
+    deleteReport: (reportName) => {
+        fileExist = fs.existsSync(`cypress/reports/${reportName}.json`)
+        if(!fileExist) {return {msg : `unable to find a report with name '${reportName}' (cypress/reports/${reportName}.json)`}}
+        fs.unlinkSync(`cypress/reports/${reportName}.json`)
+        return {status: 200, msg : `report removed succesfully`}
     }
 }
